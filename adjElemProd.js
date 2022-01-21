b3d71c5f3fcf86a7b4e76876dc9fc63f7ede1bb6
Saam Koukpari <saam.koukpari@gmail.com>
2022-01-21 15:11:04 -0800

    adjElemProd

---
+++ adjElemProd.js
@@ -0,0 +1,11 @@
+//Given an array of integers, find the pair of adjacent elements that has the largest product and return that product.
+
+function solution(inputArray) {
+  let prod = inputArray[0] * inputArray[1];
+  
+  for (let i = 1; i<inputArray.length - 1;i++) {
+      prod = Math.max(prod, inputArray[i] * inputArray[i+1]);
+  }
+  
+  return prod
+}
