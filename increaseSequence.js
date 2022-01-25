269f099dc1f4095cf4c87d0bf4a0ed2be849bca1
Saam Koukpari <saam.koukpari@gmail.com>
2022-01-25 09:46:47 -0800

    increaseSequence

---
+++ increaseSequence.js
@@ -0,0 +1,16 @@
+//Given a sequence of integers as an array, determine whether it is possible to obtain a strictly increasing sequence by removing no more than one element from the array.
+
+function solution(sequence) {
+  var count = 0;
+  
+  for(var i = 0; i < sequence.length; i++) {
+      if(sequence[i] <= sequence[i-1]) {
+          count++;
+          
+          if(sequence[i] <= sequence[i-2] && sequence[i+1] <= sequence[i-1]) return false;
+          
+      }
+  }
+  
+  return count <= 1;
+}
